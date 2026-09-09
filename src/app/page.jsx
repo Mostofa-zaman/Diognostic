ef4450739487d43989156fa32802c43099338112
mostofa zaman <mostofazaman1234@gmail.com>
2026-09-10 01:12:08 +0600

    feat(homePage): add vital divider between sections

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,17 +1,17 @@
+import VitalDivider from "@/components/common/VitalDivider";
 import CoreServices from "@/components/home/CoreServices";
 import Hero from "@/components/home/hero/Hero";
 import QuickSearch from "@/components/home/QuickSearch";
 
-
-
 export default function HomePage() {
   return (
-  <>
- <Hero/>
- <QuickSearch/>
- <CoreServices/>
-
-
-  </>
+    <>
+      <Hero />
+      <QuickSearch />
+      <CoreServices />
+      <div className="container-xl">
+        <VitalDivider color="#d3ccba" />
+      </div>
+    </>
   );
 }
